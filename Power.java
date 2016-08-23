5f85bb30f37df1213055feddb10ac0bf6cdc8e1c
SANTHOSHKUMAR U <santhoshkumaru96@gmail.com>
2016-08-23 20:34:54 +0530

    Update Power.java

--- Power.java
+++ Power.java
@@ -1,3 +1,12 @@
 import java.util.*;
 public class Power
 {
+public static void main(String args[])
+{
+  Scanner sc=new Scanner(System.in);
+  int a=sc.nextInt();
+  int b=sc.nextInt();
+  double power=Math.pow(a,b);
+  System.out.print(power);
+}
+}
