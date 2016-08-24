b7f02ffbe994d2a50ed50fd0300525ddde48fef9
SANTHOSHKUMAR U <santhoshkumaru96@gmail.com>
2016-08-24 07:20:22 +0530

    Create StringConversion.java

---
+++ StringConversion.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class StringConversion
+{
+public static void main(String args[])
+{
+Scanner sc = new Scanner(System.in);
+String s=sc.nextInt();
+int a=Integer.valueOf(s);
+System.out.print(a);
+}
+}
