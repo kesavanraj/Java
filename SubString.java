c20fffbaf75aa34a238e083dded8254df0753386
SANTHOSHKUMAR U <santhoshkumaru96@gmail.com>
2016-08-25 20:20:58 +0530

    Update SubString.java

--- SubString.java
+++ SubString.java
@@ -1,7 +1,20 @@
-import java.util.*;
-public class SubString
-{
-public static void main(String args[])
-{
-Scanner sc = new Scanner(System.in);
-String s = sc.next();
+import java.util.Scanner;
+
+public class SubString {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String s=sc.next();
+		String sub=sc.next();
+		if(s.contains(sub))
+		{
+			System.out.println("sub is substring of s");
+		}
+		else
+		{
+			System.out.println("not a substring");
+		}
+
+	}
+
+}
