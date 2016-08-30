2700b6f2b57e814d0b80d9fa820c52964d4e9e09
SANTHOSHKUMAR U <santhoshkumaru96@gmail.com>
2016-08-30 22:21:31 +0530

    Create ArrayIndex.java

---
+++ ArrayIndex.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ArrayIndex {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		for(int i=0;i<n;i++)
+		{
+			if(i == a[i])
+			{
+				System.out.println("the number "+i+" equals its index "+i);
+			}
+		}
+	}
+}
