794e25094a048bd1b296f27ee7d8b9b1b117a1c3
SANTHOSHKUMAR U <santhoshkumaru96@gmail.com>
2016-09-01 20:54:57 +0530

    Create CountWords.java

---
+++ CountWords.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+public class CountWords {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		String s1[]=s.split(" ");
+		System.out.println(s1.length);
+
+	}
+
+}
