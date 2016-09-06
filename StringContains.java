6b8c1092c26c47b34229eb247144ee134af3e599
SANTHOSHKUMAR U <santhoshkumaru96@gmail.com>
2016-09-06 07:31:59 +0530

    Create StringContains.java

---
+++ StringContains.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class StringContains
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+String s=sc.nextLine();
+String s1=sc.nextLine();
+if(s.contains(s1))
+{
+System.out.println("true");
+}
+else
+{
+System.out.println("false");
+}
+}
+}
